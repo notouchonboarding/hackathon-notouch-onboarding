913a96cc76c033c979811a826cdf1288306ebcfa
Soma Shekar Oruganti <soruganti@salesforce.com>
2018-08-27 00:54:01 -0700

    Upate the build.sh script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-tag=`git log -1 --pretty=%h``
-docker build . --tag notouchonboarding/helm:${tag} --no-cache
+tag=`git log -1 --pretty=%h`
+docker build . --tag somashekar10/helm:${tag} --no-cache
+
+docker push somashekar10/helm:${tag}
